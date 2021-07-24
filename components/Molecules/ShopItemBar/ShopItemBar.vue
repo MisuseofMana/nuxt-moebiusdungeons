b86e83baa4e9571a750af45cd0ceab0ef2c9bc5c
Sean Yager <contact@seanyager.com>
2021-07-23 19:56:04 -0500

    abstract data

--- components/Molecules/ShopItemBar/ShopItemBar.vue
+++ components/Molecules/ShopItemBar/ShopItemBar.vue
@@ -1,38 +1,42 @@
 <template>
-    <v-card hover class="d-flex align-center justify-center">
+    <v-card hover class="pa-1 d-flex align-center justify-center">
         <v-img 
-        :src="items.icon"
-        contain
-        height="60"
-        width="60"
-        max-width="60"
-        max-height="60"
+        :src="require(`@/assets/images/items/${item.iconPath}`)"
+        max-width="70"
+        max-height="70"
         class="mr-4"
         />
         <div class="d-flex flex-column text-cente mr-4">
-            <div class="text-h6"> {{ items.name }} </div>
-            <div class="text-subtitle">{{ items.description }}</div>
+            <div class="text-h6"> {{ item.name }} </div>
+            <div class="text-subtitle">{{ item.description }}</div>
         </div>
-        <Coin :wealth="items.cost"/>
+        <Coin :wealth="item.cost"/>
     </v-card>
 </template>
 
 <script>
 import clericItems from '@/mocks/items/clericItems.mock.js'
 
-    export default {
-        name: 'ShopItemBar',
-        props: {
-            shopkeep: {
-                type: String,
-                default: 'cleric'
-            },
-            items: {
-                type: Object,
-                default: () => clericItems['minorHeal']
-            }
-        },
-    }
+export default {
+	name: 'ShopItemBar',
+	data() {
+		return {
+			items: {
+				...clericItems,
+			}
+		}
+	},
+	props: {
+		shopkeep: {
+			type: String,
+			default: 'cleric'
+		},
+		item: {
+			type: Object,
+			default: () => clericItems['minorHeal']
+		}
+	},
+}
 </script>
 
 <style lang="scss" scoped>
