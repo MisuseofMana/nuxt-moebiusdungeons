a39c521539fb26e8ef0e322e0b6cc3208eb101f0
Sean Yager <contact@seanyager.com>
2021-07-23 19:52:39 -0500

    simplify contest bar

--- components/Atoms/ContestBar/ContestBar.vue
+++ components/Atoms/ContestBar/ContestBar.vue
@@ -1,56 +1,80 @@
 <template>
     <v-row>
         <v-col cols=1>
-            <v-card>
-                <v-img src=""></v-img>
-            </v-card>
+            <IconCard />
         </v-col>
         <v-col cols="10">
-            <v-progress-linear value="15"/>
-
+            <v-progress-linear :color="barColor" :value="contest"/>
         </v-col>
         <v-col cols=1>
-            
+            <IconCard />
         </v-col>
     </v-row>
 </template>
 
 <script>
-    export default {
-        name: 'ContestBar',
-        props: {
-            text: {
-                type: String,
-                default: 'Default',
-            },
-            disabled: {
-                type: Boolean,
-                default: false,
-            },
-            color: {
-                type: String,
-                default: 'primary'
-            },
-            playerStat:{
-                type: Number,
-                default: 10,
-            },
-            monsterStat: {
-                type: Number,
-                default: 5,
-            },
-            contest: {
-                type: String,
-                default: 'damage'
-            }
-        },
-        computed: {
-            barLength() {
-               const contest = this.monsterStat / this.playerStat // .5
-               const ofHundred = 100 / .5 // 200
-            }
-        }
-    }
+import { mapState } from 'vuex'
+
+export default {
+	name: 'ContestBar',
+	props: {
+		text: {
+			type: String,
+			default: 'Default',
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		color: {
+			type: String,
+			default: 'primary'
+		},
+		playerStat:{
+			type: Number,
+			default: 10,
+		},
+		monsterStat: {
+			type: Number,
+			default: 1500,
+		},
+		playerIcon: {
+			type: String,
+			default:'Attack'
+		},
+		monsterIcon: {
+			type:String,
+			default: 'Armor'
+		},
+	},
+	computed: {
+		...mapState('playerData', {
+			playerAttackType: state => state.attackType,
+			playerArmorType: state => state.armorType
+		}),
+		...mapState('monsterData', {
+			monsterAttackType: state => state.attackType,
+			monsterArmorType: state => state.armorType
+		}),
+		contest() {
+			if(this.playerStat > this.monsterStat) {
+				return ((this.monsterStat / this.playerStat) * 100) % 100
+			}
+			else if (this.monsterStat > this.playerStat) {
+				return ((this.playerStat / this.monsterStat) * 100) % 100
+			}
+			else if (this.monsterStat === this.playerStat) {
+				return 0
+			}
+			return 0
+		},
+		barColor() {
+			if (this.contest > 50) return 'green'
+			else if (this.contest < 50) return 'red'
+			return 'green'
+		},
+	}
+}
 </script>
 
 <style lang="scss" scoped>
