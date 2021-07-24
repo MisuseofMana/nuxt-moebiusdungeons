f5d0037998e0aa276d5bcffec7fd7aaba2583eb1
Sean Yager <contact@seanyager.com>
2021-07-23 19:49:48 -0500

    add mock data and store data

--- store/avatarCanvas.js
+++ store/avatarCanvas.js
@@ -1,194 +1,194 @@
 export const state = () => ({
-    characterJSON: [
-      {
-        name: 'body',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        top: 2,
-        left: 0,
-        rotation: 0,
-        max: 10,
-        lightness:0
-      },
-      {
-        name:'eyes',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        max:32,
-        lightness:0
-      },
-      {
-        name:'brows',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        max:18,
-        lightness:0
-      },
-      {
-        name:'nose',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        max: 21,
-        lightness:0
-      },
-      {
-        name:'mouth',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        max: 23,
-        lightness:0
-      },
-      {
-        name:'ears',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        top: 2,
-        left: 0,
-        rotation: 0,
-        max: 18,
-        lightness:0
-      },
-      {
-        name:'hair-front',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        top: 2,
-        left: 0,
-        rotation: 0,
-        max: 20,
-        lightness:0
-      },
-      {
-        name:'hair-back',
-        sprites: {
-          flat:[],
-          flatImg: null,
-          line:[],
-          lineImg: null,
-        },
-        which: 0,
-        max: 17,
-        lightness:0
-      },
-      {
-        name: 'extras',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        top: 0,
-        left: 0,
-        rotation:0,
-        max: 22,
-        lightness:0,
-      },
-      {
-        name:'clothes',
-        sprites: {
-          flat:[],
-          flatImg:null,
-          line:[],
-          lineImg:null,
-        },
-        which: 0,
-        top: 2,
-        left: 0,
-        rotation: 0,
-        max: 13,
-        lightness:0
-      },
-    ],
+	characterJSON: [
+		{
+			name: 'body',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			top: 2,
+			left: 0,
+			rotation: 0,
+			max: 10,
+			lightness:0
+		},
+		{
+			name:'eyes',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			max:32,
+			lightness:0
+		},
+		{
+			name:'brows',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			max:18,
+			lightness:0
+		},
+		{
+			name:'nose',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			max: 21,
+			lightness:0
+		},
+		{
+			name:'mouth',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			max: 23,
+			lightness:0
+		},
+		{
+			name:'ears',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			top: 2,
+			left: 0,
+			rotation: 0,
+			max: 18,
+			lightness:0
+		},
+		{
+			name:'hair-front',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			top: 2,
+			left: 0,
+			rotation: 0,
+			max: 20,
+			lightness:0
+		},
+		{
+			name:'hair-back',
+			sprites: {
+				flat:[],
+				flatImg: null,
+				line:[],
+				lineImg: null,
+			},
+			which: 0,
+			max: 17,
+			lightness:0
+		},
+		{
+			name: 'extras',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			top: 0,
+			left: 0,
+			rotation:0,
+			max: 22,
+			lightness:0,
+		},
+		{
+			name:'clothes',
+			sprites: {
+				flat:[],
+				flatImg:null,
+				line:[],
+				lineImg:null,
+			},
+			which: 0,
+			top: 2,
+			left: 0,
+			rotation: 0,
+			max: 13,
+			lightness:0
+		},
+	],
 })
   
 export const mutations = {
-  REPLACE_CHARACTER_JSON(state, payload) {
-    state.characterJSON = payload
-  },
-  POPULATE_SPRITE_ARRAY(state, payload) {
-    state.characterJSON[payload.index].sprites[payload.arrayName] = payload.loadedPaths
-  },
-  INCREMENT_CHARACTER_OPTION(state, index) {
-    state.characterJSON[index].which++
-  },
-  DECREMENT_CHARACTER_OPTION(state, index) {
-    state.characterJSON[index].which--
-  },
-  SET_CHARACTER_OPTION(state, payload) {
-    state.characterJSON[payload.index].which = payload.set
-  }
+	REPLACE_CHARACTER_JSON(state, payload) {
+		state.characterJSON = payload
+	},
+	POPULATE_SPRITE_ARRAY(state, payload) {
+		state.characterJSON[payload.index].sprites[payload.arrayName] = payload.loadedPaths
+	},
+	INCREMENT_CHARACTER_OPTION(state, index) {
+		state.characterJSON[index].which++
+	},
+	DECREMENT_CHARACTER_OPTION(state, index) {
+		state.characterJSON[index].which--
+	},
+	SET_CHARACTER_OPTION(state, payload) {
+		state.characterJSON[payload.index].which = payload.set
+	}
 }
 
 export const actions = {
-  characterOptionUp({state, commit}, index) {
-    const itemObject = state.characterJSON[index]
+	characterOptionUp({state, commit}, index) {
+		const itemObject = state.characterJSON[index]
     
-    // if selection number is not max increase which
-    if(itemObject.which < itemObject.max - 1) {
-      commit('INCREMENT_CHARACTER_OPTION', index)
-    }
-    // otherwise set which to -1
-    else {
-      commit('SET_CHARACTER_OPTION', {index, set: -1})
-    }
-  },
-  characterOptionDown({state, commit}, index) {
-    const itemObject = state.characterJSON[index]
+		// if selection number is not max increase which
+		if(itemObject.which < itemObject.max - 1) {
+			commit('INCREMENT_CHARACTER_OPTION', index)
+		}
+		// otherwise set which to -1
+		else {
+			commit('SET_CHARACTER_OPTION', {index, set: -1})
+		}
+	},
+	characterOptionDown({state, commit}, index) {
+		const itemObject = state.characterJSON[index]
 
-    // if selection number is not -1
-    if (itemObject.which > -1) {
-      commit('DECREMENT_CHARACTER_OPTION', index)
-    }
-    // otherwise set which to max
-    else {
-      commit('SET_CHARACTER_OPTION', {index, set: itemObject.max - 1})
-    }
-  }
+		// if selection number is not -1
+		if (itemObject.which > -1) {
+			commit('DECREMENT_CHARACTER_OPTION', index)
+		}
+		// otherwise set which to max
+		else {
+			commit('SET_CHARACTER_OPTION', {index, set: itemObject.max - 1})
+		}
+	}
 }
 
 export const getters = {
-  character_selections(state) {
-    let whiches = []
-    state.characterJSON.forEach((item) => {
-      whiches.push(item.which)
-    })
-    return whiches
-  }
+	character_selections(state) {
+		let whiches = []
+		state.characterJSON.forEach((item) => {
+			whiches.push(item.which)
+		})
+		return whiches
+	}
 }
---
+++ store/shopkeepData.js
@@ -0,0 +1,54 @@
+export const state = () => ({
+	visiting: 'cleric',
+	shopkeeps: {
+		cleric: {
+			description: 'A devout matron of the Eyien church. Heals your wounds, provides blessings, buffs, and powerful boons.',
+			saying:'Come in, are you hurt?',
+			shopTitle: 'I can heal you... or perhaps you need the favor of the old gods?',
+			welcome:['hello', 'hello2', 'welcome', 'welcome2',],
+			welcomeBack:['youreBack', 'youreBack2' ],
+			goodbye:['goodbye', 'goodbye2', 'beCareful', 'beCareful2', 'seeYouSoon', 'seeYouSoon2', 'staySafe', 'staySafe2' ],
+			thankYou:['feelingBetter', 'feelingBetter2', 'thanks', 'thanks2', 'thankYou', 'thankYou2', ],
+			cantBuy:['notEnough', 'notEnough2', 'moreCoin', 'moreCoin2', ],
+			bigBuy:['byAmara', 'byAmara2']
+		},
+		graverobber: {
+			description: 'An untrustworthy soul. Provides items that come with a deeper cost than just simple coins. Don\'t get him wrong, he\'s happy to take your coin too.',
+			saying:'Trust me, nothing I sell is cursed.',
+			shopTitle: 'Just buy somethin\' quick, I don\'t wanna be seen fencing to the Kingloyal.',
+			welcome:['heyThere', 'heyThereCoterie', 'heyThereCoterie2', 'huh', 'keepYourDistance', 'wantSome', 'welcome', 'whatDoYouNeed', 'whatDoYouNeed2', 'whatDoYouNeed3'],
+			welcomeBack:['backForMore', 'backForMore2', 'backForMore3', 'enoughOfMe', 'enoughOfMe2', 'enoughOfMe3', 'knewYoudBeBack', 'knewYoudBeBack2',], 
+			goodbye:['illBeHere', 'illBeHere2', 'nextTime', 'nextTime2', 'okay', 'staySafe', 'staySafe2'],
+			thankYou:['alright', 'buyAnother', 'buyAnother2', 'buyAnother3', 'goodEye', 'goodEye2', 'heresOne', 'laugh', 'laugh2', ],
+			cantBuy:['noBargaining', 'noBargaining2', 'noDiscounts', 'notEnoughCoin', 'notEnoughCoin2', 'notEnoughCoin3', 'youreBroke'],
+			bigBuy:['gladToRidThat', 'gladYouBought', 'gladYouBought2', 'pickedAnother', 'pickedAnother2', 'thankYouSire',]
+		},
+		merchant: {
+			description: 'An honest tradesman. Provides meals to raise your max Health, kits to increase your Armor, and Mettle vials to embolden your special abilities.',
+			saying:'Friend or foe, what are ya\' buyin\'?',
+			shopTitle: 'It\'s not much, but it\'s what I\'ve got. All priced to move.',
+			welcome:['hello', 'howsTheAdventuring', 'newStockToday', 'takeALookAround', 'welcome'],
+			welcomeBack:['backForMore', 'welcomeBack'], 
+			goodbye:['seeYouNextTime', 'staySafe'],
+			thankYou:['anythingElse', 'goodChoice', 'interestingChoice', 'justPickedThatOut', 'thankYou'],
+			cantBuy:['areYouKidding', 'shortSomeCoin', 'shortSomeCoin2', 'cantAffordThat', 'trySomethingCheaper', 'outOfYourMind', 'notACharity','goLootSomething', 'needMoreCoin', 'needMoreCoin2'],
+			bigBuy:['justCleanedOff', 'spendItAllAtOnce', 'wonderfulChoice']
+		},
+		witch: {
+			description: 'A primordial being, unassuming in shape. She offers powerful spells to wildly boost your stats. Be wary, some magics can be more trouble than they\'re worth.',
+			saying:'Everything you see, all hand enchanted.',
+			shopTitle: 'Some of my inventory tends to be virulent. Browse at your own risk.',
+			welcome:['hello', 'hello2', 'hello3', 'oh', 'ohHello' ],
+			welcomeBack:['welcomeBack', 'welcomeBack2'], 
+			goodbye:['goodbye', 'goodbye2', 'seeYouSoon', 'seeYouSoon2'],
+			thankYou:['thankYou', 'thankYou2', 'carefulWithThat', 'carefulWithThat2', 'itsAllYours', 'itsAllYours2'],
+			cantBuy:['notEnough', 'notEnough2'],
+			bigBuy:['interestingChoice', 'interestingChoice2', ]
+		}
+	},
+	shopLoaded: undefined,
+	info: {},
+	shopChoice: [],
+	inventory: [],
+	visited:[],
+})
